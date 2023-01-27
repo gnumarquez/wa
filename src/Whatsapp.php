9d4c7b4a192c4263c3dc0299c28420caec5f29f4
Wilmer Márquez <gnumarquez@gmail.com>
2023-01-27 11:24:18 -0400

    fix

--- src/Whatsapp.php
+++ src/Whatsapp.php
@@ -63,8 +63,14 @@ Class Whatsapp {
 				$data['btn'] = $this->btn;
 			}
 
+			$url = "https://api.whatsapp506.biz/sendOne";
+			if ($this->btn){
+				$data['btn'] = $this->btn;
+				$url = "https://api.whatsapp506.biz/sendOneBtn";
+			}
+
 			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL,"https://api.whatsapp506.biz/sendOne");
+			curl_setopt($ch, CURLOPT_URL,$url);
 			curl_setopt($ch, CURLOPT_POST, TRUE);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
